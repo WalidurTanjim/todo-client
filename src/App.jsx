defc8095d832513896c0a462050095724305a208
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-02-20 21:14:59 +0600

    ToDo app basec setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-red-500 text-2xl font-semibold'>ToDo Website for SCIC assignment</h1>
+    </>
+  )
+}
+
+export default App
